9284e15185c015fabb33032b4306ea9aa205c729
maks2 <maks210306@yandex.by>
2024-12-17 17:41:32 +0300

    Если б было море пиво, я б дельфином стал крассивым

--- src/account/account.cpp
+++ src/account/account.cpp
@@ -1,38 +1,3 @@
-#ifndef REPETITORPLATFORM_ACCOUNT_H
-#define REPETITORPLATFORM_ACCOUNT_H
-
-#include "../main/mainwin.h"
-#include <QWidget>
-#include "../basewin/basewin.h"
-#include "../repetitors/Repetitor.h" // Подключаем класс репетитора
-#include <QMap> // Для хранения предметов и репетиторов
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class account; }
-QT_END_NAMESPACE
-
-class account : public BaseWin { // Наследуемся от BaseWin
-Q_OBJECT
-
-public:
-    explicit account(QWidget *parent = nullptr);
-    ~account();
-
-private:
-    Ui::account *ui;
-    QMap<QString, QVector<QString>> subjectRepetitors; // Карта: предмет -> список репетиторов
-
-    void loadRepetitorsFromFile(const QString &fileName); // Загрузка данных
-    void saveRepetitorsToFile(const QString &fileName);   // Сохранение данных
-    void displayItems();                                  // Отображение предметов и репетиторов
-
-private slots:
-    void onDeleteButtonClicked();
-    void onReturnButtonClicked();
-};
-
-#endif //REPETITORPLATFORM_ACCOUNT_H
-
 #include "account.h"
 #include "ui_account.h"
 #include <QMessageBox>
--- src/exception/ErrorBase.h
+++ src/exception/ErrorBase.h
@@ -1,22 +1,22 @@
-#ifndef ERRORBASE_H
-#define ERRORBASE_H
+    #ifndef ERRORBASE_H
+    #define ERRORBASE_H
 
-#include <QString>
-#include <QMessageBox>
-#include <QDebug>
+    #include <QString>
+    #include <QMessageBox>
+    #include <QDebug>
 
-class ErrorBase {
-protected:
-    QString message; // Описание ошибки
-    QString context; // Контекст ошибки
+    class ErrorBase {
+    protected:
+        QString message; // Описание ошибки
+        QString context; // Контекст ошибки
 
-public:
-    ErrorBase(const QString &message, const QString &context = QString());
-    virtual ~ErrorBase() = default;
+    public:
+        ErrorBase(const QString &message, const QString &context = QString());
+        virtual ~ErrorBase() = default;
 
-    virtual void showError() const = 0; // Абстрактный метод для отображения ошибки
-    QString getMessage() const;
-    QString getContext() const;
-};
+        virtual void showError() const = 0; // Абстрактный метод для отображения ошибки
+        QString getMessage() const;
+        QString getContext() const;
+    };
 
-#endif // ERRORBASE_H
+    #endif // ERRORBASE_H
